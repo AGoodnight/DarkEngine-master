233b24aeda1ed37da1d01a38e3ac1f763cc7ee0d
Adam <chaofoam@gmail.com>
2014-07-02 15:24:06 -0400

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,25 +1,3 @@
 function debug(t){
 	console.log(t.str)
 }
-
-function validate(input){
-
-	var c = thisgame.cmnds;
-	var inCmnds = false;
-
-	for( j in c){
-		if(c[j].str === input){
-			c[j].func();
-			inCmnds = true;
-		}
-	}
-	
-	if(!inCmnds){
-		if(!!thisgame.usr.inventory){
-			findIn(thisgame.usr.inventory,input);
-		}else{
-			console.log("Error: You must create an inventory on the usr object");
-		}
-	}
-	
-}
