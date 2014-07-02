6985a88e88efdd2cc14fcf0de0a7241faea42289
Adam <mediag@mg-mm-002.mediag.com>
2014-07-02 15:23:30 -0400

    Added Functions and Objects
    Validate, Foes

--- js/master-revised.js
+++ js/master-revised.js
@@ -1,35 +1,64 @@
 function game(params){
 	
-	this.cmnds={
-		//single words
-		inventory:{str:'i',func:function(){debug(this)}},
-		help:{str:'help',func:function(){debug(this)}},
-		//conjuctive words
-		take:{str:'take',func:function(){debug(this)}},
-		drop:{str:'drop',func:function(){debug(this)}},
-		inspect:{str:'inspect',func:function(){debug(this)}},
-		look:{str:'look',func:function(){debug(this)}}
-	};
-	
 	this.defaults={
 		level:1,
 		health:50,
 		ambushChance:.5 // chance of being ambushed
 	};
 	
+	this.cmnds={
+	
+		//single words
+		inventory:{str:'i',func:function(){debug(this)}},
+		help:{str:'help',func:function(){debug(this)}},
+		
+		//conjuctive words
+		take:{str:'take',func:function(){debug(this)}},
+		drop:{str:'drop',func:function(){debug(this)}},
+		inspect:{str:'inspect',func:function(){debug(this)}},
+		look:{str:'look',func:function(){debug(this)}}	
+			
+	};
+	
+	this.validate = function(input){
+			var c = this.cmnds;
+			var inCmnds = false;
+
+			for( j in c){
+				if(c[j].str === input){
+					c[j].func();
+					inCmnds = true;
+				}
+			};
+	
+			if(!inCmnds){
+				if(!!this.usr.inventory){
+					findIn(this.usr.inventory,input);
+				}else{
+					console.log("Error: You must create an inventory on the usr object");
+				}
+			};
+			
+			return this;
+	
+		}
+	
 	this.data={
+		curArea:[null,null], //longitude and latitude
 		inBattle:false,
 		foeAttacking:false,
 		gameOver:false
 	};
 	
 	this.usr = {
-		cmndLog:[] // log the last 50 commands executed by the user
+		cmndLog:[], // log the last few commands executed by the user
+		attack:function(){},
+		stats:{}
 	};
 	
-	this.mob = {
-		cur:{}, // the current foe
-		log:[], // log the last 20 foes
+	this.foes = {
+		cur:{}, // the current foe, an instantiated JSON object
+		log:[], // log the last few foes
 		clear:function(){
 			this.log.push(this.cur);
 			this.cur = undefined;
@@ -52,22 +81,12 @@ function game(params){
 			}
 		}
 		
-	}
-	
+	}	
 	this.set(params,this.defaults);
 	
 	return this
 	
 }
 
-var thisgame = new game({health:100});
-
-var c = thisgame.cmnds;
-
-var input = 'help'
-
-for( j in c){
-	if(c[j].str === input){
-		c[j].func();
-	}
-}
+var w = new game({health:100});
+w.validate('help');
--- js/util.js
+++ js/util.js
@@ -1,3 +1,25 @@
 function debug(t){
 	console.log(t.str)
 }
+
+function validate(input){
+
+	var c = thisgame.cmnds;
+	var inCmnds = false;
+
+	for( j in c){
+		if(c[j].str === input){
+			c[j].func();
+			inCmnds = true;
+		}
+	}
+	
+	if(!inCmnds){
+		if(!!thisgame.usr.inventory){
+			findIn(thisgame.usr.inventory,input);
+		}else{
+			console.log("Error: You must create an inventory on the usr object");
+		}
+	}
+	
+}
